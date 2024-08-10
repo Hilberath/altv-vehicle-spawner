4b1f1bd4d9839cb20b5c1d72858091ffb7363646
David Hilberath <David.Hilberath@icloud.com>
2024-08-10 11:17:08 +0200

    Upload

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # altv-vehicle-spawner
+
+## English
+
 A tool for spawning vehicles on an AltV server.
+
+### How to install?
+
+1. Unpack .rar file
+2. Put 'altv-vehicle-spawner' folder to your 'resources' folder
+3. in server.toml add 'altv-vehicle-spawner' in "resources"
+
+### Screenshots
+
+![alt text](https://i.imgur.com/ixgYPAa.png)
+![alt text](https://i.imgur.com/wckvY2a.png)
+
+### How do I open the menu?
+
+- You can open the menu by pressing the "F9" key.
+- You can close the menu by pressing the "F9" or "Esc" key.
+- You can change the key bindings in the client.js file, lines 51-58.
+
+## Deutsch
+
+Ein Tool zum Spawnen von Fahrzeugen auf einem AltV-Server.
+
+### Wie installiert man es?
+
+- Die .rar-Datei entpacken
+- Ordner 'altv-vehicle-spawner' in den 'resources'-Ordner verschieben
+- In server.toml 'altv-vehicle-spawner' zu "resources" hinzufügen
+
+### Bilder
+
+![alt text](https://i.imgur.com/ixgYPAa.png)
+![alt text](https://i.imgur.com/wckvY2a.png)
+
+### Wie öffne ich das Menü?
+
+- Du kannst das Menü öffnen, indem du die "F9"-Taste drückst.
+- Du kannst das Menü schließen, indem du die "F9"- oder "Esc"-Taste drückst.
+- Du kannst die Tastenbelegung in der Datei client.js in den Zeilen 51-58 ändern.
---
+++ client.js
@@ -0,0 +1,68 @@
+import * as alt from "alt";
+import * as native from "natives";
+
+let loaded = false;
+let opened = false;
+
+const view = new alt.WebView("http://resource/html/index.html");
+
+function menu(toggle) {
+  opened = toggle;
+
+  alt.showCursor(toggle);
+  alt.toggleGameControls(!toggle);
+
+  if (toggle) {
+    view.focus();
+  } else {
+    view.unfocus();
+  }
+
+  view.emit("menu", toggle);
+}
+
+function promisify(callback) {
+  return new Promise((resolve, reject) => {
+    let loader = alt.setInterval(() => {
+      if (callback() == true) {
+        resolve(true);
+        alt.clearInterval(loader);
+      }
+    }, 80);
+  });
+}
+
+view.on("ready", () => {
+  loaded = true;
+});
+
+view.on("menu", (toggle) => {
+  menu(toggle);
+});
+
+view.on("select", (model) => {
+  alt.emitServer("playerSpawnVehicle", model);
+  menu(false);
+});
+
+alt.on("keyup", (key) => {
+  if (!loaded) return;
+
+  if (key === 0x69) {
+    // Numpad 9 Taste
+    menu(!opened);
+  } else if (opened && key === 0x1b) {
+    // Escape-Taste
+    menu(false);
+  }
+});
+
+alt.onServer("setPedIntoVehicle", async (vehicle) => {
+  const player = alt.Player.local;
+  await promisify(() => {
+    if (player.vehicle) return true;
+    native.setPedIntoVehicle(player.scriptID, vehicle.scriptID, -1);
+  });
+});
+
+alt.log("[HVS] Client-Side Loaded.");
